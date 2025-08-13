fe1518d4f9799c1695c3acddd27e0fab29148531
Majawat <12058855+Majawat@users.noreply.github.com>
2025-08-13 21:33:30 +0000

    just tiny update to test github tags

--- src/main.cpp
+++ src/main.cpp
@@ -13,8 +13,8 @@ SoftwareSerial audioSerial(D7, D6); // RX=D7(GPIO20), TX=D6(GPIO21)
 DFRobotDFPlayerMini dfPlayer;
 
 // Firmware version
-#define FIRMWARE_VERSION "0.16.0"
-#define VERSION_FEATURE "Add dual weapon system with synchronized audio/LED effects"
+#define FIRMWARE_VERSION "0.16.1"
+#define VERSION_FEATURE "GitHub Tag test"
 #define BUILD_DATE __DATE__ " " __TIME__
 
 // Web server and WiFi
